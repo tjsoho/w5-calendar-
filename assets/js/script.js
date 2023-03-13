6f625bf94548a42a33c826f03c624470a8f5c532
Tjcarroll1 <tjcarroll1@icloud.com>
2023-03-14 08:36:40 +1000

    added color code loops and save and retrieving from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,8 @@
 // TODO: Add code to display the current date in the header of the page.
 
 var timeDisplayEl = $('#currentDay');
-var saveCalEntry = $('saveBtn')
-var textEl = $('textarea').val()
+var saveCalEntry = $('.saveBtn')
+
 
 function displayTime() {
   var rightNow = dayjs().format('MMM DD, YYYY [at] h:mm a');
@@ -34,9 +34,44 @@ displayTime();
 
 function saveLocal() {
   saveCalEntry.on('click', function () {
-    localStorage.setItem('calendarEntry', textEl);
+    const value = $(this).siblings('.description').val()
+    const key = $(this).parent().attr('id')
+    localStorage.setItem(key, value);
   })
 }
 saveLocal()
 
+function loadLocal() {
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'))
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'))
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'))
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'))
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'))
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'))
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'))
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'))
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'))
+}
+loadLocal()
 
+function colorCode() {
+  $('.time-block').each(function () {
+    const hour = parseInt($(this).attr('id').split('-')[1]);
+    const curHour = dayjs().hour();
+    $(this).removeClass('past')
+    $(this).removeClass('present')
+    $(this).removeClass('future')
+    if (hour < curHour) {
+      $(this).addClass('past')
+    }
+    else if (hour === curHour) {
+      $(this).addClass('present');
+    }
+    else {
+      $(this).addClass('future');
+    }
+  })
+}
+colorCode()
+
+setInterval(colorCode, 3600000)
